2838f8c750cb3b3b0543f2310042fd98d250afbb
JHaley1513 <34946424+JHaley1513@users.noreply.github.com>
2022-02-07 08:21:00 -0800

    Merge sort not-in-place version start

--- merge_sort.py
+++ merge_sort.py
@@ -8,6 +8,7 @@ def merge1(arr, l, m, r):
     # we assume arr[l:d] and arr[m + 1:r] are already sorted, then merge to create arr[l:r].
     # num elements merged: r - l.
     print(f'merge1({l}, {m}, {r})')
+
     print('\t', end='')
     array_print(arr)
     i = k = l
@@ -41,14 +42,53 @@ def merge_sort1(arr, l, r):
     merge1(arr, l, m, r)
 
 
-# def merge2(arr, l, m, r):
+def merge2(arr, sub_arr1, sub_arr2):
+    """Merge two sorted subarrays into one combined sorted array."""
+    i = j = 0 # index in sub_arr1 and sub_arr2
+    while i + j < len(arr):
+        # This way you'll usually end up with `list index out of range`
+        if sub_arr1[i] < sub_arr2[j]:
+            arr[i + j] = sub_arr1[i]
+            i += 1
+        else:
+            arr[i + j] = sub_arr2[j]
+            j += 1
+        array_print(arr)
 
-# def merge_sort2(arr, l, r):
+
+def merge_sort2(arr):
+    n = len(arr)
+    if n < 2:
+        return
+    mid = n // 2
+
+    arr1 = arr[:mid]
+    arr2 = arr[mid:n]
+    merge_sort2(arr1)
+    merge_sort2(arr2)
+
+    l1 = len(arr1)
+    l2 = len(arr2)
+    # Best case A: largest in arr1 is smaller than smallest in arr2, so all of arr1 items < all of arr2 items
+    if arr1[-1] < arr2[0]:
+        for i in range(l1):
+            arr[i] = arr1[i]
+        for i in range(l2):
+            arr[l1 + i] = arr2[i]
+    # Best case B: all of arr1 items > all of arr2 items
+    elif arr1[0] > arr2[-1]:
+        for i in range(l2):
+            arr[i] = arr2[i]
+        for i in range(l1):
+            arr[l2 + i] = arr1[i]
+    # Normal case
+    else:
+        merge2(arr, arr1, arr2)
 
 if __name__=='__main__':
     a = array_random()
     array_print(a)
     try:
-        merge_sort1(a, 0, len(a))
+        merge_sort2(a)
     finally:
         array_print(a)
